3ce3050390142625aed0d8b0a45131248c0d3aa0
Priyatham Kattakinda <priyathamkat@gmail.com>
2022-11-20 14:06:15 -0500

    Define a basic

--- Sources/SwiftML/SwiftML.swift
+++ Sources/SwiftML/SwiftML.swift
@@ -1,6 +1,15 @@
-public struct SwiftML {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
+public class Tensor<T: Numeric> {
+    public let shape: [UInt64]
+    public let size: UInt64
+    
+    private var data: [T]
+    
+    init(ofShape shape: [UInt64]) {
+        self.shape = shape
+        let size = shape.reduce(1, { x, y in
+            x * y
+        })
+        self.size = size
+        self.data = Array(repeating: 0, count: Int(truncatingIfNeeded: size))
     }
 }
