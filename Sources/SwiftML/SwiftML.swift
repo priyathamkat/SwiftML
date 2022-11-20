6dbf95a748135572b85440639921c205d7b44bca
Priyatham Kattakinda <priyathamkat@gmail.com>
2022-11-20 17:39:55 -0500

    Add constructors for initializing with zeros and ones

--- Sources/SwiftML/SwiftML.swift
+++ Sources/SwiftML/SwiftML.swift
@@ -1,15 +1,47 @@
-public class Tensor<T: Numeric> {
+public class TensorShape {
     public let shape: [Int]
-    public let size: Int
-    
-    private var data: [T]
-    
-    init(ofShape shape: [Int]) {
-        self.shape = shape
-        let size = shape.reduce(1, { x, y in
-            x * y
+    public var size: Int {
+        self.shape.reduce(1, { d_i, d_j in
+            d_i * d_j
         })
-        self.size = size
-        self.data = Array<T>(repeating: 0, count: size)
+    }
+    public var ndim: Int {
+        self.shape.count
+    }
+    
+    public init(_ shape: [Int]) {
+        self.shape = shape
+    }
+}
+
+public class Tensor {
+    public let shape: TensorShape
+    public var size: Int {
+        self.shape.size
+    }
+    public var ndim: Int {
+        self.shape.ndim
+    }
+    
+    private var data: [Float]
+    
+    private init(ofShape shape: TensorShape, withData data: [Float]) {
+        assert(shape.size == data.count, "Size of data doesn't match the size inferred from shape")
+        self.shape = shape
+        self.data = data
+    }
+    
+    convenience init(withZerosOfShape shape: [Int]) {
+        let shape = TensorShape(shape)
+        var data = Array<Float>(repeating: 0, count: shape.size)
+        
+        self.init(ofShape: shape, withData: data)
+    }
+    
+    convenience init(withOnesOfShape shape: [Int]) {
+        let shape = TensorShape(shape)
+        var data = Array<Float>(repeating: 1, count: shape.size)
+        
+        self.init(ofShape: shape, withData: data)
     }
 }
