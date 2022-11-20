5a38ef408a4aee25630d0f41a438369878e1390c
Priyatham Kattakinda <priyathamkat@gmail.com>
2022-11-20 18:12:57 -0500

    Compare tensors in tests

--- Tests/SwiftMLTests/SwiftMLTests.swift
+++ Tests/SwiftMLTests/SwiftMLTests.swift
@@ -7,9 +7,11 @@ final class SwiftMLTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
         let x = Tensor(withZerosOfShape: [2, 2])
+        let ones = Tensor(withOnesOfShape: [2, 2])
+        
         XCTAssertEqual(x.shape, TensorShape([2, 2]))
-        XCTAssertEqual((x + 1).data, [1, 1, 1, 1])
-        XCTAssertEqual((x - 1).data, [-1, -1, -1, -1])
-        XCTAssertEqual((1 - x).data, [1, 1, 1, 1])
+        XCTAssertEqual((x + 1), ones)
+        XCTAssertEqual((x - 1), -ones)
+        XCTAssertEqual((1 - x), ones)
     }
 }
