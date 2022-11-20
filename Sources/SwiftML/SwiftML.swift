aad0ae5f539f790853065c02bf19c7eab5930ae7
Priyatham Kattakinda <priyathamkat@gmail.com>
2022-11-20 18:20:40 -0500

    Implement scalar multiplication

--- Sources/SwiftML/SwiftML.swift
+++ Sources/SwiftML/SwiftML.swift
@@ -72,8 +72,8 @@ extension Tensor {
         return right + left
     }
     
-    public static prefix func - (t: Tensor) -> Tensor {
-        return Tensor(ofShape: t.shape, withData: vDSP.negative(t.data))
+    public static prefix func - (tensor: Tensor) -> Tensor {
+        return Tensor(ofShape: tensor.shape, withData: vDSP.negative(tensor.data))
     }
     
     public static func - (left: Tensor, right: Float) -> Tensor {
@@ -84,5 +84,15 @@ extension Tensor {
     
     public static func - (left: Float, right: Tensor) -> Tensor {
         return -right + left
+    }
+    
+    public static func * (left: Tensor, right: Float) -> Tensor {
+        let shape = left.shape
+        let data: [Float] = vDSP.multiply(right, left.data)
+        return Tensor(ofShape: shape, withData: data)
+    }
+    
+    public static func * (left: Float, right: Tensor) -> Tensor {
+        return right * left
     }
 }
--- Tests/SwiftMLTests/SwiftMLTests.swift
+++ Tests/SwiftMLTests/SwiftMLTests.swift
@@ -6,12 +6,13 @@ final class SwiftMLTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        let x = Tensor(withZerosOfShape: [2, 2])
+        let zeros = Tensor(withZerosOfShape: [2, 2])
         let ones = Tensor(withOnesOfShape: [2, 2])
         
-        XCTAssertEqual(x.shape, TensorShape([2, 2]))
-        XCTAssertEqual((x + 1), ones)
-        XCTAssertEqual((x - 1), -ones)
-        XCTAssertEqual((1 - x), ones)
+        XCTAssertEqual(zeros.shape, TensorShape([2, 2]))
+        XCTAssertEqual((zeros + 1), ones)
+        XCTAssertEqual((zeros - 1), -ones)
+        XCTAssertEqual((1 - zeros), ones)
+        XCTAssertEqual(2 * ones, zeros + 2)
     }
 }
