fa4f1b2f596a24e3d31c0d3c3a7116c052e095dd
Priyatham Kattakinda <priyathamkat@gmail.com>
2022-11-20 22:46:16 -0500

    Implement creating random uniform and gaussian tensors

--- Tests/SwiftMLTests/SwiftMLTests.swift
+++ Tests/SwiftMLTests/SwiftMLTests.swift
@@ -16,4 +16,30 @@ final class SwiftMLTests: XCTestCase {
         XCTAssertEqual(2 * ones, zeros + 2)
         XCTAssertEqual(ones / 2, zeros + 0.5)
     }
+    
+    func testUniformPerformance() throws {
+        let metrics: [XCTMetric] = [XCTClockMetric()]
+        let measureOptions = XCTMeasureOptions.default
+        measureOptions.iterationCount = 100
+        
+        measure(metrics: metrics, options: measureOptions) {
+            let a = Float.random(in: 0..<1)
+            let b = Float.random(in: 0..<1)
+            let x = Tensor(withUniformOfShape: [10000, 1000])
+            let _ = a * x + b
+        }
+    }
+    
+    func testGaussianPerformance() throws {
+        let metrics: [XCTMetric] = [XCTClockMetric()]
+        let measureOptions = XCTMeasureOptions.default
+        measureOptions.iterationCount = 100
+        
+        measure(metrics: metrics, options: measureOptions) {
+            let a = Float.random(in: 0..<1)
+            let b = Float.random(in: 0..<1)
+            let x = Tensor(withGaussianOfShape: [10000, 1000])
+            let _ = a * x + b
+        }
+    }
 }
